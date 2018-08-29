214a8193040f4853c2f1d5249014ca1b6f96b2be
Daniel O'Rourke <42179758+danlong22@users.noreply.github.com>
2018-08-29 09:33:44 +0800

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,4 +1,6 @@
 def collatz(uInput):
+	#change the string returned by the input() function to an integer
+	uInput = int(uInput)
 	print(uInput)
 	#creates the Collatz sequence
 	while uInput != 1:
@@ -9,9 +11,18 @@ def collatz(uInput):
 		print(uInput)
 	#prevents window from closing before the user can see the output
 	input('Press Enter to exit.')
-
-number = int(input('Please Enter an integer.\n'))
-collatz(number)
+#checks to make sure that the user input is valid
+finished = False
+while finished == False:
+	number = input('Please Enter an integer.\n')
+	try:
+		collatz(number)
+		break
+	except:
+		print('That is not an integer.')
+		number = input('Please Enter an integer.\n')
+		
+		
 
 
 
